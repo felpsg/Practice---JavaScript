64d96762324c32b04fcce077f6beba0d2eb30a15
Felipe Gonçalves <75172718+felpsg@users.noreply.github.com>
2021-11-03 23:50:13 -0300

    Add files via upload

---
+++ converterFahrenheitemCesius.js
@@ -0,0 +1,10 @@
+const kelvin = 255; 
+const Celsius = kelvin - 273;
+let fahrenheit = Celsius *(9/5) +32;
+fahrenheit = Math.floor(fahrenheit); 
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+
+let newton = Celsius *(33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
