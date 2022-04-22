f4b0b1a3b0bc56ae9b92f8ccc996ca5fca6b478e
Felipe Gonçalves <felipetec.sso@gmail.com>
2022-04-22 02:11:31 -0300

    Refazendo os exercicios de logica!

--- Exercício3.js
+++ Exercício3.js
@@ -1,34 +1,59 @@
-let userName = 'Felipe';
-userName ? console.log('Hello, $userName'):
-console.log("Hello!");
-let userQuestion = "Can you answer my question";
-console.log('The user asked:${userQuestion}');
-let randomNumber = Math.floor(Math.random() * 8);
- let eightBall ='';
+let userName = "Felipe";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
- switch(randomNumber){
-   case 0:
-   eightBall = 'It is certain';
-   break;
- case 1:
-   eightBall = 'It is decidedly so';
-   break;
- case 2:
-   eightBall = 'Reply hazy try again';
-   break;
- case 3:
-    eightBall = 'Cannot predict now';
-  break;
+let userQuestion = " O que você deseja fazer ao Magic Eight Ball?";
+console.log(`${userName} has asked - ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
   case 4:
-    eightBall = 'Do not count on it';
-  break;
+    eightBall = "Do not count on it";
+    break;
   case 5:
-    eightBall = 'My sources say no';
-  break;
+    eightBall = "My sources say no";
+    break;
   case 6:
-    eightBall = 'Outlook not so good';
+    eightBall = "Outlook not so good";
     break;
   case 7:
-    eightBalll = 'Signs point to yes';
- }
+    eightBalll = "Signs point to yes";
+}
 console.log(eightBall);
+
+
+/*
+if (randomNumber === 'eightBall') {
+  console.log('It is certain');
+} else {
+  console.log('It is decidedly so');
+} if (randomNumber === 'eightBall') {
+  console.log('Reply hazy try again');
+} else {
+  console.log('Cannot predict now');
+} if (randomNumber === 'eightBall') {
+  console.log('Do not count on it');
+}else {
+  console.log('My sources say no');
+} if (randomNumber === 'eightBall') {
+  console.log('Outlook not so good');
+} else {
+  console.log('Signs point to yes');
+}
+
+console.log(eightBall); /*
--- MagicEightBall.js
+++ MagicEightBall.js
@@ -1,26 +1,32 @@
-let userName = "Felipe"
+let userName = "Felipe";
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
 let userQuestion = " O que você deseja fazer ao Magic Eight Ball?"
-console.log(`Desejo tirar o número da sorte!'${userQuestion}`);
+console.log(`${userName} has asked - ${userQuestion}`);
 
-const randomNumber = Math.floor(Math.random() * 8);
-let eightBall = ""
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
 
-switch (randomNumber){
-  case 0:
-  eightBall = 'It is certain';
-  break;
-  case 1:
-  eightBall = 'It is certain';
-  break;
-  case 2:
-  eightBall = 'Reply hazy try again';
-  break;
-  case 3:
-  eightBall = 'Cannot predict now';
-  break;
-  case 4:
-  eightBall = 'Do not count on it';
-  break;
-  console.log("eightBall");
+let eightBall = "";
+
+if (randomNumber === 'eightBall') {
+  console.log('It is certain');
+} else {
+  console.log('It is decidedly so');
+} if (randomNumber === 'eightBall') {
+  console.log('Reply hazy try again');
+} else {
+  console.log('Cannot predict now');
+} if (randomNumber === 'eightBall') {
+  console.log('Do not count on it');
+}else {
+  console.log('My sources say no');
+} if (randomNumber === 'eightBall') {
+  console.log('Outlook not so good');
+} else {
+  console.log('Signs point to yes');
+}
+
+console.log(eightBall);
+// exemplo de resolução swith abaixo.
+
