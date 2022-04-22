6ab6c06fdeeb7c14c0c82d9e2cd4a082d4a6981e
Felipe Gonçalves <felipetec.sso@gmail.com>
2022-04-22 06:27:26 -0300

    Update MagicEightBall.js

--- MagicEightBall.js
+++ MagicEightBall.js
@@ -1,7 +1,7 @@
 let userName = "Felipe";
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
-let userQuestion = " O que você deseja fazer ao Magic Eight Ball?"
+let userQuestion = " O que você deseja fazer ao Magic Eight Ball?";
 console.log(`${userName} has asked - ${userQuestion}`);
 
 let randomNumber = Math.floor(Math.random() * 8);
@@ -9,24 +9,22 @@ console.log(randomNumber);
 
 let eightBall = "";
 
-if (randomNumber === 'eightBall') {
-  console.log('It is certain');
+if (randomNumber === "eightBall") {
+  eightBall = "It is certain";
+} else if (randomNumber === "spring") {
+  eightBall = "It is decidedly so";
+} else if (randomNumber === "basic") {
+  eightBall = "Reply hazy try again";
+} else if (randomNumber === "uper") {
+  eightBall = "Cannot predict now";
+} else if (randomNumber === "casoc") {
+  eightBall = "Do not count on it";
+} else if (randomNumber === "basicc") {
+  eightBall = "My sources say no";
+} else if (randomNumber === "rajh") {
+  eightBall = "Outlook not so good";
 } else {
-  console.log('It is decidedly so');
-} if (randomNumber === 'eightBall') {
-  console.log('Reply hazy try again');
-} else {
-  console.log('Cannot predict now');
-} if (randomNumber === 'eightBall') {
-  console.log('Do not count on it');
-}else {
-  console.log('My sources say no');
-} if (randomNumber === 'eightBall') {
-  console.log('Outlook not so good');
-} else {
-  console.log('Signs point to yes');
+  eightBall = "Signs point to yes";
 }
 
 console.log(eightBall);
-// exemplo de resolução swith abaixo.
-
