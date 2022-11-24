7b60a099df9e8f59645987ca952fbb7b6f798223
felps <felipetec.sso@gmail.com>
2022-11-24 19:15:06 -0300

    work function

---
+++ cc.js
@@ -0,0 +1,18 @@
+//HIGHER-ORDER FUNCTIONS
+//Functions as Parameters
+
+const addTwo = (num) => {
+  return num + 2;
+};
+
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 2;
+  let checkB = func(val);
+  //return checkA === checkB ? func(val) : 'Erros Inconsistente';
+  if (checkA === checkB) {
+    return func(val);
+  } else 'Erros Inconsistente';
+};
+console.log(checkConsistentOutput(addTwo, 6));
+
+
