6d88f7a7300c20cf73164cf85f2c41846850f1ab
felps <felipetec.sso@gmail.com>
2022-12-25 17:54:03 -0300

    refatorando css

--- galeria-simples-emJS/script.js
+++ galeria-simples-emJS/script.js
@@ -12,3 +12,36 @@ function galeriaClique(imagem) {
 }
 
 imagens.forEach(galeriaClique);
+
+
+const imagensGaleria = galeria.querySelectorAll("li img");
+
+imagens.forEach(imagem => {
+  imagem.addEventListener("mouseenter", () => {
+    imagem.style.opacity = "0.7";
+    imagem.style.transform = "scale(1.1)";
+  });
+  imagem.addEventListener("mouseleave", () => {
+    imagem.style.opacity = "1";
+    imagem.style.transform = "scale(1)";
+  });
+});
+
+const galeria = document.querySelector("#galeria");
+const lightbox = document.querySelector("#lightbox");
+const imagemLightbox = lightbox.querySelector("img");
+const fechar = lightbox.querySelector("button");
+
+galeria.addEventListener("click", e => {
+  if (e.target.tagName === "IMG") {
+    lightbox.style.display = "flex";
+    imagemLightbox.src = e.target.src;
+  }
+});
+
+fechar.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+
+
